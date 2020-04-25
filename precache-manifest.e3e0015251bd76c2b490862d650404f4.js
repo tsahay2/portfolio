3adcc4c3937ae7b3a33ecff8337b93765d317769
tushar sahay <tsahay2@gmail.com>
2020-04-26 04:33:52 +0530

    Updates

---
+++ precache-manifest.e3e0015251bd76c2b490862d650404f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4146144a36d797253c1f1f30d7128808",
+    "url": "/portfolio-tush/index.html"
+  },
+  {
+    "revision": "0c964d23d95c3be1d6f4",
+    "url": "/portfolio-tush/static/css/main.c20499bd.chunk.css"
+  },
+  {
+    "revision": "e2d04311507e3a4ff2cd",
+    "url": "/portfolio-tush/static/js/2.51374d8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-tush/static/js/2.51374d8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c964d23d95c3be1d6f4",
+    "url": "/portfolio-tush/static/js/main.c7a14ebe.chunk.js"
+  },
+  {
+    "revision": "e56f06d8c21b606050df",
+    "url": "/portfolio-tush/static/js/runtime-main.2b237c24.js"
+  },
+  {
+    "revision": "5b3c19a9bef371f7ec28d8d0dc1ef8aa",
+    "url": "/portfolio-tush/static/media/profile.5b3c19a9.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e678ca91cb1187280656cfb27b438931.js"
+  "/portfolio-tush/precache-manifest.e3e0015251bd76c2b490862d650404f4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-tush/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
